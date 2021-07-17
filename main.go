8399b97f9d1e7d1f9f3a1d37f2fb7af96a374e65
Sunimal Herath <sunimaldv@gmail.com>
2021-07-18 00:55:59 +1000

    adding books template and go mod

--- main.go
+++ main.go
@@ -1,6 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+var tpl *template.Template
+
+type Book struct {
+	isbn  string
+	title string
+	price float32
+}
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
 
 func main() {
 	http.HandleFunc("/", index)
@@ -15,4 +32,5 @@ func index(w http.ResponseWriter, req *http.Request) {
 
 func booksIndex(w http.ResponseWriter, req *http.Request) {
 	// execute template books.gohtml
+	tpl.ExecuteTemplate(w, "books.gohtml", nil)
 }
