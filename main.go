b03095fe0b9dbaa9cd623041b079394c6e2d4d89
sunimalherath <sunimaldv@gmail.com>
2021-07-22 00:15:47 +1000

    displaying all books

--- main.go
+++ main.go
@@ -9,17 +9,19 @@ import (
 )
 
 var tpl *template.Template
+var db *sql.DB
 
 type Book struct {
-	isbn  string
-	title string
-	price float32
+	Isbn  string
+	Title string
+	Price float32
 }
 
 func init() {
+	var err error
 	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 
-	db, err := sql.Open("postgres", "postgres://bookeeper:pass123@localhost/bookstore?sslmode=disable")
+	db, err = sql.Open("postgres", "postgres://johndoe:pass123@localhost/bookstore?sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
@@ -38,6 +40,32 @@ func index(w http.ResponseWriter, req *http.Request) {
 }
 
 func booksIndex(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+	}
+	// display all books in books.gohtml
+	rows, err := db.Query("SELECT * FROM books")
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+	}
+	defer rows.Close()
+
+	books := make([]Book, 0)
+
+	for rows.Next() {
+		book := Book{}
+		rows.Scan(&book.Isbn, &book.Title, &book.Price)
+		if err != nil {
+			http.Error(w, http.StatusText(500), 500)
+		}
+		books = append(books, book)
+	}
+
+	if err = rows.Err(); err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
 	// execute template books.gohtml
-	tpl.ExecuteTemplate(w, "books.gohtml", nil)
+	tpl.ExecuteTemplate(w, "books.gohtml", books)
 }
